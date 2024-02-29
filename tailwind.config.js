073123a5f7c5c8195822cc9e1394d9dd1b676bde
jcli-ie <jclie-ie@proton.me>
2024-02-29 09:02:45 +0000

    Revamp search, hero and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/*.templ'],
+  content: ["./views/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        surface: "#FFFEFF",
+        base: "#F2F4F8",
+        subtle: "#E5E5E6",
+        red: "#FC5353",
+        blue: "#888BFD",
+        text: "#231F20",
+      },
+      backgroundSize: {
+        "50%": "50%",
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography'), require("daisyui")],
-}
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
